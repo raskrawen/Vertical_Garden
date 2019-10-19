fa1bc1bdc43759d4669c9f97dbe7558d8d066263
Rasmus Kragh Wendelbo <29890659+raskrawen@users.noreply.github.com>
2019-10-19 09:35:09 +0200

    Update secrets.h

--- readFromThingspeak/secrets.h
+++ readFromThingspeak/secrets.h
@@ -2,9 +2,9 @@
 // and connection details
 
 #define SECRET_SSID "camilla-PC-Wireless"		// replace MySSID with your WiFi network name
-#define SECRET_PASS "n33gvsn33gvs"	// replace MyPassword with your WiFi password
+#define SECRET_PASS ""	// replace MyPassword with your WiFi password
 
 #define SECRET_CH_ID_WEATHER_STATION 12397	          	//MathWorks weather station
 
 #define SECRET_CH_ID_COUNTER 256551 					//Test channel for counting
-#define SECRET_READ_APIKEY_COUNTER "4TP0JZRPOARZD46J"	//API Key for Test channel
+#define SECRET_READ_APIKEY_COUNTER ""	//API Key for Test channel
