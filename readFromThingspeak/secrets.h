654a57c4c32a0296d2db3c6b5963a1a93d8c84fe
Rasmus Kragh Wendelbo <29890659+raskrawen@users.noreply.github.com>
2019-10-19 09:31:29 +0200

    Add files via upload

---
+++ readFromThingspeak/secrets.h
@@ -0,0 +1,10 @@
+// Use this file to store all of the private credentials 
+// and connection details
+
+#define SECRET_SSID "camilla-PC-Wireless"		// replace MySSID with your WiFi network name
+#define SECRET_PASS "n33gvsn33gvs"	// replace MyPassword with your WiFi password
+
+#define SECRET_CH_ID_WEATHER_STATION 12397	          	//MathWorks weather station
+
+#define SECRET_CH_ID_COUNTER 256551 					//Test channel for counting
+#define SECRET_READ_APIKEY_COUNTER "4TP0JZRPOARZD46J"	//API Key for Test channel
